d5aebc69e22f0f52685d977b2f2b01a434a7904c
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-04-02 21:30:01 +0200

    feat: add profile component

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/auth/auth.service";
+import { User } from "src/app/models/user/entities/User";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.component.html",
+  styleUrls: ["./user-profile.component.scss"],
 })
 export class UserProfileComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.user = this.authService.currentUser;
+    console.log("this.user", this.user);
   }
-
 }
