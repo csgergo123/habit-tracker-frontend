bc7c6ab9ac808b3f775e44afdd7e15b9a2345d87
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-04-02 18:51:07 +0200

    feat: add logout to sidebar too

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
 
 declare interface RouteInfo {
   path: string;
@@ -43,7 +44,7 @@ export class SidebarComponent implements OnInit {
   public menuItems: any[];
   public isCollapsed = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
     this.menuItems = ROUTES.filter((menuItem) => menuItem);
