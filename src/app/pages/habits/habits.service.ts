4e87fea0fbaa8ff6120da3e57fc669b2ad6abfa0
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-04-02 18:10:20 +0200

    refactor: remove light or dark examination from habit

--- src/app/pages/habits/habits.service.ts
+++ src/app/pages/habits/habits.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
+import { NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { AuthService } from "src/app/auth/auth.service";
@@ -28,40 +28,25 @@ export class HabitsService {
 
   fetchHabits(): Observable<Habit[]> {
     const httpOptions = this.getAuthHeader();
-    return this.http.get<Habit[]>(`${env.backendUrl}/habits`, httpOptions).pipe(
-      map((habits: Habit[]) => {
-        habits.map((habit) => {
-          habit.lightOrDark = this.lightOrDark(habit.color);
-        });
-        return habits;
-      })
-    );
+    return this.http.get<Habit[]>(`${env.backendUrl}/habits`, httpOptions);
   }
 
   fetchHabitsToBeDone(): Observable<Habit[]> {
     const httpOptions = this.getAuthHeader();
-    return this.http
-      .get<Habit[]>(`${env.backendUrl}/habits/to-be-done`, httpOptions)
-      .pipe(
-        map((habits: Habit[]) => {
-          habits.map((habit) => {
-            habit.lightOrDark = this.lightOrDark(habit.color);
-          });
-          return habits;
-        })
-      );
+    return this.http.get<Habit[]>(
+      `${env.backendUrl}/habits/to-be-done`,
+      httpOptions
+    );
   }
 
   createHabit(habit: Habit): Observable<Habit> {
+    console.log("habit", habit);
     const httpOptions = this.getAuthHeader();
-    return this.http
-      .post<Habit>(`${env.backendUrl}/habits`, habit, httpOptions)
-      .pipe(
-        map((habit) => {
-          console.log(habit);
-          return habit;
-        })
-      );
+    return this.http.post<Habit>(
+      `${env.backendUrl}/habits`,
+      habit,
+      httpOptions
+    );
   }
 
   habitDone(habitId: number) {
@@ -78,43 +63,7 @@ export class HabitsService {
     return this.http.delete(`${env.backendUrl}/habits/${habitId}`, httpOptions);
   }
 
-  private lightOrDark(color) {
-    if (!color) {
-      return "light";
-    }
-
-    // Variables for red, green, blue values
-    var r, g, b, hsp;
-
-    // Check the format of the color, HEX or RGB?
-    if (color.match(/^rgb/)) {
-      // If RGB --> store the red, green, blue values in separate variables
-      color = color.match(
-        /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/
-      );
-
-      r = color[1];
-      g = color[2];
-      b = color[3];
-    } else {
-      // If hex --> Convert it to RGB: http://gist.github.com/983661
-      color = +(
-        "0x" + color.slice(1).replace(color.length < 5 && /./g, "$&$&")
-      );
-
-      r = color >> 16;
-      g = (color >> 8) & 255;
-      b = color & 255;
-    }
-
-    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
-    hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
-
-    // Using the HSP value, determine whether the color is light or dark
-    if (hsp > 127.5) {
-      return "light";
-    } else {
-      return "dark";
-    }
+  formatDate(date: NgbDateStruct | null): string {
+    return date ? date.year + "-" + date.month + "-" + date.day : "";
   }
 }
