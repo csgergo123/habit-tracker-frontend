722787cd754895d2485ba363419d683147bce3cd
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-04-10 15:15:31 +0200

    feat: add habit dones chart to dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import Chart from "chart.js";
 import {
   chartOptions,
   parseOptions,
-  chartExample1,
+  colors,
   chartExample2,
 } from "../../variables/charts";
 import { HabitsService } from "../habits/habits.service";
@@ -19,21 +19,87 @@ import { TodosService } from "../todos/todos.service";
 export class DashboardComponent implements OnInit {
   numberOfHabits: number;
   numberOfHabitsToDo: number;
+  numberOfhabitsDoneToday: number;
   numberOfTodos: number;
   numberOfUndoneTodos: number;
+  numberOfTodosDoneToday: number;
+
+  dailyHabitDonesForThisWeek;
+  weeklyHabitDonesForThisMonth;
+
+  habitChart: any;
+
   isFetching = false;
 
   public datasets: any;
   public data: any;
-  public salesChart;
+  public habitsChart;
   public clicked: boolean = true;
   public clicked1: boolean = false;
 
   constructor(
     private habitService: HabitsService,
     private todoService: TodosService
-  ) {
-    this.isFetching = true;
+  ) {}
+
+  ngOnInit() {
+    this.initStatNumbers();
+
+    this.getHabitChart();
+
+    this.datasets = [
+      this.dailyHabitDonesForThisWeek,
+      this.weeklyHabitDonesForThisMonth,
+    ];
+    this.data = this.dailyHabitDonesForThisWeek;
+
+    var chartOrders = document.getElementById("chart-orders");
+
+    parseOptions(Chart, chartOptions());
+
+    /*var chartOrders = document.getElementById("chart-orders");
+
+    var ordersChart = new Chart(chartOrders, {
+      type: "bar",
+      options: chartExample2.options,
+      data: chartExample2.data,
+    });*/
+
+    var chartHabits = document.getElementById("chart-habits");
+
+    this.habitsChart = new Chart(chartHabits, {
+      type: "line",
+      options: this.habitChart.options,
+      data: this.habitChart.data,
+    });
+  }
+
+  // public updateOptions() {
+  //   this.habitsChart.data.datasets[0].data = this.data;
+  //   this.habitsChart.update();
+  // }
+
+  public updatDaily() {
+    this.habitChart.data.datasets[0].data = this.dailyHabitDonesForThisWeek;
+    this.habitChart.data.labels = [
+      "Mon",
+      "Tue",
+      "Wed",
+      "Thu",
+      "Fri",
+      "Sat",
+      "Sun",
+    ];
+    this.habitsChart.update();
+  }
+
+  public updateWeekly() {
+    this.habitChart.data.datasets[0].data = this.weeklyHabitDonesForThisMonth;
+    this.habitChart.data.labels = ["1", "2", "3", "4"];
+    this.habitsChart.update();
+  }
+
+  private initStatNumbers() {
     this.habitService.fetchHabits().subscribe((habits) => {
       this.numberOfHabits = habits.length;
     });
@@ -46,36 +112,50 @@ export class DashboardComponent implements OnInit {
     this.todoService.fetchUndoneTodos().subscribe((todos) => {
       this.numberOfUndoneTodos = todos.length;
     });
+
+    this.habitService
+      .getDailyHabitDonesForThisWeek()
+      .subscribe((dailyHabitDones) => {
+        console.log("dailyHabitDones", dailyHabitDones);
+        this.dailyHabitDonesForThisWeek = dailyHabitDones;
+        this.updatDaily();
+      });
+    this.habitService
+      .getWeeklyHabitDonesForThisMonth()
+      .subscribe((weeklyHabitDone) => {
+        this.weeklyHabitDonesForThisMonth = [1, 2, 3, 4];
+        //this.habitChart.data.datasets[0].data = weeklyHabitDone;
+      });
+
+    this.habitChart = {
+      options: {
+        scales: {
+          yAxes: [
+            {
+              gridLines: {
+                color: colors.gray[900],
+                zeroLineColor: colors.gray[900],
+              },
+              ticks: {
+                callback: function (value) {
+                  return value;
+                },
+              },
+            },
+          ],
+        },
+      },
+      data: {
+        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        datasets: [
+          {
+            label: "Done habits",
+            data: [0, 0, 0, 0, 0, 0, 0],
+          },
+        ],
+      },
+    };
   }
 
-  ngOnInit() {
-    this.datasets = [
-      [0, 20, 10, 30, 15, 40, 20, 60, 60],
-      [0, 20, 5, 25, 10, 30, 15, 40, 40],
-    ];
-    this.data = this.datasets[0];
-
-    var chartOrders = document.getElementById("chart-orders");
-
-    parseOptions(Chart, chartOptions());
-
-    var ordersChart = new Chart(chartOrders, {
-      type: "bar",
-      options: chartExample2.options,
-      data: chartExample2.data,
-    });
-
-    var chartSales = document.getElementById("chart-sales");
-
-    this.salesChart = new Chart(chartSales, {
-      type: "line",
-      options: chartExample1.options,
-      data: chartExample1.data,
-    });
-  }
-
-  public updateOptions() {
-    this.salesChart.data.datasets[0].data = this.data;
-    this.salesChart.update();
-  }
+  private getHabitChart() {}
 }
