70faed593ac920ecc11b88e9ff2aad0a55040a23
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-04-10 18:07:22 +0200

    test: rename tests for order

---
+++ e2e/src/06-profile.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { browser } from "protractor";
+import { DashboardPage } from "./dashboard.po";
+
+describe("Habit tracker - Dashboard", () => {
+  let dashboardPage: DashboardPage;
+
+  beforeEach(() => {
+    dashboardPage = new DashboardPage();
+  });
+
+  it("should access to Dashboard page", () => {
+    dashboardPage.navigateTo();
+    expect(dashboardPage.getTitleText()).toEqual("DASHBOARD");
+  });
+
+  it("should all habit number more than 0", () => {
+    // Cast Promise<string> to number
+    ((expect(dashboardPage.getAllNumberOfHabitsForToday()) as any) as number) >
+      0;
+  });
+
+  it("should habit to do number is 0", () => {
+    // Cast Promise<string> to number
+    ((expect(dashboardPage.getNumberOfHabitsToDo()) as any) as number) == 0;
+  });
+
+  it("should all todos number is nore than 0", () => {
+    // Cast Promise<string> to number
+    ((expect(dashboardPage.getAllTodosForToday()) as any) as number) == 0;
+  });
+
+  it("should undone todos number is 0", () => {
+    // Cast Promise<string> to number
+    ((expect(dashboardPage.getAllTodosForToday()) as any) as number) > 0;
+  });
+});
