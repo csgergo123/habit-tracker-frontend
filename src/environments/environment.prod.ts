197f0add6a5752714c8f041853fc597fa66e7719
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-04-13 19:49:31 +0200

    fix: remove slash from backend domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: "https://de-habit-tracker-backend.herokuapp.com/",
+  backendUrl: "https://de-habit-tracker-backend.herokuapp.com",
 };
